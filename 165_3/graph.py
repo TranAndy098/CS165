c584a378294cd87cc342d95d08929be92955a3a0
Andy PC <trana@DESKTOP-TGR6CNB.(none)>
2023-05-29 14:03:10 -0700

    everything is done except optimal clustering and eros graph

--- 165_3/graph.py
+++ 165_3/graph.py
@@ -5,16 +5,28 @@ from collections.abc import Iterable
 
 class Graph:
 	def __init__(self, num_nodes: int, edges: Iterable[tuple[int, int]]):
-		pass
+		self.num_nodes = num_nodes
+		self.num_edges = 0
+		self.adj_list = []
+		for i in range(num_nodes):
+			self.adj_list.append(list())
+		for edge in edges:
+			self.num_edges += 1
+			self.adj_list[edge[0]].append(edge[1])
+			self.adj_list[edge[1]].append(edge[0])
+
 
 	def get_num_nodes(self) -> int:
-		pass
+		return self.num_nodes
 
 	def get_num_edges(self) -> int:
-		pass
+		return self.num_edges
 
 	def get_neighbors(self, node: int) -> Iterable[int]:
-		pass
+		if node > self.num_nodes - 1:
+			return []
+		return self.adj_list[node]
+
 
 	# feel free to define new methods in addition to the above
 	# fill in the definitions of each required member function (above),
